d4af95c7c6f319872c6f8e14675314753ee82d90
yuzhengmin <3236170161@qq.com>
2026-08-07 17:56:26 +0800

    follow

--- src/sciv/tool/_scvmap_.py
+++ src/sciv/tool/_scvmap_.py
@@ -3,7 +3,13 @@
 import requests
 from requests import Response
 
+from .. import util as ul
+
 from ..util import scvmap_url
+
+__name__: str = "tool_scvmap"
+
+log = ul.log(__name__)
 
 
 def get_result_data(resp: Response):
@@ -16,10 +22,12 @@ def get_result_data(resp: Response):
 
 
 def request_get_data(path: str, **kwargs):
+    log.info(f"Request {scvmap_url}/{path}")
     response = requests.get(f"{scvmap_url}/{path}", **kwargs)
     return get_result_data(response)
 
 
 def request_post_data(path: str, json: dict = None, **kwargs):
+    log.info(f"Request {scvmap_url}/{path}")
     response = requests.post(f"{scvmap_url}/{path}", json=json, **kwargs)
     return get_result_data(response)
